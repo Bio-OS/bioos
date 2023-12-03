c9f922bf900e59b362250643bab3d00f71186b1f
Bing64 <625255264@qq.com>
2023-12-03 12:43:25 +0800

    Fix for CWL parser

--- internal/context/workspace/infrastructure/workflowparser/cwlparser.go
+++ internal/context/workspace/infrastructure/workflowparser/cwlparser.go
@@ -10,12 +10,13 @@ import (
 	"path"
 	"regexp"
 	"strings"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/Bio-OS/bioos/internal/context/workspace/interface/grpc/proto"
 	apperrors "github.com/Bio-OS/bioos/pkg/errors"
 	applog "github.com/Bio-OS/bioos/pkg/log"
 	"github.com/Bio-OS/bioos/pkg/utils/exec"
-	"gopkg.in/yaml.v2"
 )
 
 type CWLConfig struct {
@@ -74,7 +75,7 @@ func (cwl *CWLParser) ValidateWorkflowFiles(ctx context.Context, baseDir, mainWo
 		return "", err
 	}
 	depsResultLines := strings.Split(string(depsResults), "\n")
-	if len(validateResultLines) < 3 {
+	if len(depsResultLines) < 3 {
 		return "", proto.ErrorWorkflowValidateError("fail to check deps")
 	}
 	jsonContent := strings.Join(depsResultLines[2:], "\n")
@@ -110,49 +111,69 @@ func (cwl *CWLParser) ValidateWorkflowFiles(ctx context.Context, baseDir, mainWo
 
 func (cwl *CWLParser) GetWorkflowInputs(ctx context.Context, WorkflowFilePath string) (string, error) {
 	params := make([]WorkflowParam, 0)
-	templateResult, err := exec.Exec(ctx, CommandExecuteTimeout, cwl.Config.CwltoolCmd, "--make-template", WorkflowFilePath)
+	rdfResult, err := exec.Exec(ctx, CommandExecuteTimeout, cwl.Config.CwltoolCmd, "--print-rdf", WorkflowFilePath)
 	if err != nil {
 		return "", err
 	}
-	templateResultLines := strings.Split(string(templateResult), "\n")
-	yamlContent := strings.Join(templateResultLines[2:], "\n")
 
-	var yamlData map[string]interface{}
-	if err := yaml.Unmarshal([]byte(yamlContent), &yamlData); err != nil {
-		return "", err
+	inputsRe := regexp.MustCompile(`(?s)cwl:inputs\s(.*?)\s;`) // 找到所有输入文件
+	inputs := inputsRe.FindAllStringSubmatch(string(rdfResult), -1)
+	var inputFiles []string
+	if len(inputs) > 0 {
+		for _, value := range inputs {
+			files := strings.Split(value[1], ",")
+			for _, file := range files {
+				trimmedFile := strings.TrimSpace(file)
+				inputFiles = append(inputFiles, trimmedFile)
+			}
+		}
+	} else {
+		return "", nil
 	}
 
-	for key, value := range yamlData {
-		param := WorkflowParam{Name: key}
+	sectionRe := regexp.MustCompile(`[\s]*\r?\n[\s]*\r?\n[\s]*`) // 输出结果分块
+	rdfResultSection := sectionRe.Split(string(rdfResult), -1)
 
-		keyWithColon := key + ":"
-		startIndex := strings.Index(yamlContent, keyWithColon)
-		if startIndex == -1 {
-			continue
+	for _, value := range rdfResultSection { // 遍历各个分块，找到描述对应文件的部分
+		if containsAny(value, inputFiles) && !strings.Contains(value, "cwl:inputs") {
+			param := WorkflowParam{}
+			nameRe := regexp.MustCompile(`#(.*?)> `)
+			nameMatch := nameRe.FindStringSubmatch(value)
+			if len(nameMatch) > 1 {
+				param.Name = nameMatch[1]
+				lastSlashIndex := strings.LastIndex(param.Name, "/") // 某些情况下会多一级斜杠
+				if lastSlashIndex != -1 {
+					param.Name = param.Name[lastSlashIndex+1:]
+				}
+			} else {
+				continue
+			}
+
+			param.Optional = strings.Contains(value, "sld:null")
+
+			typeRe := regexp.MustCompile(`sld:type\s+.*?([^:\s,.]+)[\s,.]`)
+			typeMatch := typeRe.FindAllStringSubmatch(value, -1)
+			if len(typeMatch) > 0 {
+				if strings.Contains(typeMatch[0][1], "[") { // Array Enum Record等类型，文本格式与其他类型不同
+					param.Type = upperCaseFirstLetter(typeMatch[len(typeMatch)-1][1])
+				} else {
+					param.Type = upperCaseFirstLetter(typeMatch[0][1])
+				}
+			} else {
+				continue
+			}
+
+			defaultRe := regexp.MustCompile(`cwl:default\s+.*?([^;]+)\s`)
+			defaultMatch := defaultRe.FindStringSubmatch(value)
+			if len(defaultMatch) > 1 && param.Type != "Record" { // Record类型暂时无法输出默认值
+				if param.Type == "Array" {
+					fmt.Println("[" + removeSpacesAndNewlines(defaultMatch[1]) + "]")
+				} else {
+					param.Default = defaultMatch[1]
+				}
+			}
+			params = append(params, param)
 		}
-		endIndex := strings.Index(yamlContent[startIndex:], "\n")
-		var paramDesc string
-		if endIndex == -1 {
-			paramDesc = yamlContent[startIndex:]
-		} else {
-			paramDesc = yamlContent[startIndex:endIndex]
-		}
-
-		reType := regexp.MustCompile(`type\s+'([^']*)'`)
-		matchType := reType.FindStringSubmatch(paramDesc)
-		if matchType != nil {
-			param.Type = matchType[1]
-		} else {
-			continue
-		}
-
-		param.Optional = strings.Contains(paramDesc, "(optional)")
-
-		if strings.Contains(paramDesc, "default value") {
-			param.Default = value.(string)
-		}
-
-		params = append(params, param)
 	}
 
 	return workflowParamDoToPO(params)
@@ -164,26 +185,56 @@ func (cwl *CWLParser) GetWorkflowOutputs(ctx context.Context, WorkflowFilePath s
 	if err != nil {
 		return "", err
 	}
-	rdfResultSection := strings.Split(string(rdfResult), "\n\n")
-	for _, value := range rdfResultSection {
-		if strings.Contains(value, "cwl:outputBinding") {
-			param := WorkflowParam{Optional: false}
-			re := regexp.MustCompile(`sld:type\s+([^:\s]+):`)
-			match := re.FindStringSubmatch(value)
-			if len(match) > 1 {
-				param.Type = match[1]
+
+	outputsRe := regexp.MustCompile(`(?s)cwl:outputs\s(.*?)\s;`) // 找到所有输出文件
+	outputs := outputsRe.FindAllStringSubmatch(string(rdfResult), -1)
+	var outputFiles []string
+	if len(outputs) > 0 {
+		for _, value := range outputs {
+			files := strings.Split(value[1], ",")
+			for _, file := range files {
+				trimmedFile := strings.TrimSpace(file)
+				outputFiles = append(outputFiles, trimmedFile)
+			}
+		}
+	} else {
+		return "", nil
+	}
+
+	sectionRe := regexp.MustCompile(`[\s]*\r?\n[\s]*\r?\n[\s]*`) // 输出结果分块
+	rdfResultSection := sectionRe.Split(string(rdfResult), -1)
+
+	for _, value := range rdfResultSection { // 遍历各个分块，找到描述对应文件的部分
+		if containsAny(value, outputFiles) && !strings.Contains(value, "cwl:outputs") {
+			param := WorkflowParam{}
+			nameRe := regexp.MustCompile(`#(.*?)> `)
+			nameMatch := nameRe.FindStringSubmatch(value)
+			if len(nameMatch) > 1 {
+				param.Name = nameMatch[1]
+				lastSlashIndex := strings.LastIndex(param.Name, "/") // 某些情况下会多一级斜杠
+				if lastSlashIndex != -1 {
+					param.Name = param.Name[lastSlashIndex+1:]
+				}
 			} else {
 				continue
 			}
 
-			re2 := regexp.MustCompile(`\/(.*?)> rdfs:comment`)
-			match2 := re2.FindStringSubmatch(value)
-			if len(match2) > 1 {
-				param.Name = match[1]
+			param.Optional = true // 输出全部为Optional
+
+			typeRe := regexp.MustCompile(`sld:type\s+.*?([^:\s,.]+)[\s,.]`)
+			typeMatch := typeRe.FindAllStringSubmatch(value, -1)
+			if len(typeMatch) > 0 {
+				if strings.Contains(typeMatch[0][1], "[") { // Array Enum Record等类型，文本格式与其他类型不同
+					param.Type = upperCaseFirstLetter(typeMatch[len(typeMatch)-1][1])
+				} else {
+					param.Type = upperCaseFirstLetter(typeMatch[0][1])
+				}
 			} else {
 				continue
 			}
-			param.Default = value
+
+			// 不处理default
+
 			params = append(params, param)
 		}
 	}
@@ -203,4 +254,29 @@ type CWLFile struct {
 	Basename       string    `json:"basename,omitempty"`       // omitempty保证如果字段为空，则在JSON中省略
 	Nameroot       string    `json:"nameroot,omitempty"`
 	Nameext        string    `json:"nameext,omitempty"`
+}
+
+func containsAny(str string, substrings []string) bool {
+	for _, substr := range substrings {
+		if strings.Contains(str, substr) {
+			return true
+		}
+	}
+	return false
+}
+
+func upperCaseFirstLetter(s string) string {
+	if s == "" {
+		return ""
+	}
+	r, size := utf8.DecodeRuneInString(s)
+	return string(unicode.ToUpper(r)) + s[size:]
+}
+
+func removeSpacesAndNewlines(input string) string {
+	parts := strings.Split(input, ",")
+	for i, part := range parts {
+		parts[i] = strings.TrimSpace(part)
+	}
+	return strings.Join(parts, ",")
 }
